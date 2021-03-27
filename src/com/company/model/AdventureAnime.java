db432b88947c0f91d7ad087dd57cfd8a347ae216
Evgeniy Fando <fando.evgeniy@gmail.com>
2021-03-27 16:28:16 +0300

    fixed class

--- src/com/company/model/AdventureAnime.java
+++ src/com/company/model/AdventureAnime.java
@@ -4,8 +4,8 @@ import com.company.Statistics;
 
 public class AdventureAnime extends Anime {
 
-
-    public AdventureAnime(String name, Statistics statistics, AgeLimits limit, String description, Tag tag) {
-        super (name, statistics, limit, description, tag);
+    public AdventureAnime(final String name, final Statistics statistics, final AgeLimits limit,
+                          final String description, final Tag tag) {
+        super(name, statistics, limit, description, tag);
     }
 }
